051d944904940ac5999573cc8e8706d8a26b5c82
Andrey Bakuta <dra1n86@gmail.com>
2013-05-24 13:48:23 +0300

    Copy bin files after gem was installed.

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,18 +1,28 @@
 require 'rubygems'
 require 'rubygems/command.rb'
+require 'fileutils'
+require 'rbconfig'
 
 begin
   Gem::Command.build_args = ARGV
 rescue NoMethodError
 end
 
-def get_binary_gemspec(platform = RUBY_PLATFORM)
-  eval(File.read('wkhtmltopdf_for_rails.gemspec'))
+platform = RbConfig::CONFIG['host_os']
+cpu = RbConfig::CONFIG['host_cpu']
+
+if platform =~ /linux/ && cpu =~ /[3|4]86/
+  executable = 'wkhtmltopdf_linux'
+elsif platform =~ /linux/ && cpu =~ /64/
+  executable = 'wkhtmltopdf_amd64'
+elsif platform =~ /darwin/
+  executable = 'wkhtmltopdf_darwin'
+else
+  raise "Invalid platform. Must be running linux or intel-based Mac OS."
 end
 
-gemspec = get_binary_gemspec
-FileUtils.mkdir_p 'bin'
-FileUtils.cp "ext/wkhtmltopdf/wkhtmltopdf_#{gemspec.platform.os}", "bin/wkhtmltopdf"
+FileUtils.mkdir_p '../bin'
+FileUtils.cp "wkhtmltopdf/#{executable}", "../bin/wkhtmltopdf"
 
 f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
 f.write("task :default\n")
